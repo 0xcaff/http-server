6475ccc67a0f92e432c28d26a5f5f3b051445dcf
Martin Charles <martincharles07@gmail.com>
2016-05-27 12:05:06 -0400

    Added Header Customization and Better Binding
    Also improved color printing.
    
    Closes #3
    Closes #1

--- main.go
+++ main.go
@@ -5,23 +5,35 @@ import (
 	"fmt"
 	"github.com/koding/logging"
 	"net/http"
+	"strings"
 )
 
 var (
-	port = flag.String("port", "8080", "Port to use")
-	path = flag.String("path", "./", "Path the server is started from")
+	listen = flag.String("listen", ":8080", "Address to listen for requests on")
+	path   = flag.String("path", "./", "Path the server is started from")
+	header = flag.String("header", "", "Header sent with every response")
 )
 
 func main() {
 	flag.Parse()
 
+	var headers map[string]string
+	if *header != "" {
+		s := strings.Split(*header, ":")
+		headers = map[string]string{
+			s[0]: s[1],
+		}
+	}
+
 	server := HTTPServer{
 		http.Server{
-			Addr: ":" + *port,
+			Addr: *listen,
 			Handler: HTTPHandler{
 				realHandler: http.FileServer(http.Dir(*path)),
+				headers:     headers,
 			},
 		},
+		headers,
 	}
 
 	err := server.ListenAndServe()
@@ -32,26 +44,41 @@ func main() {
 
 type HTTPHandler struct {
 	realHandler http.Handler
+	headers     map[string]string
 }
 
 func (h HTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	logging.Info("%s %s", r.Method, r.URL)
 
+	responseHeaders := w.Header()
+	for k, v := range h.headers {
+		responseHeaders.Set(k, v)
+	}
 	h.realHandler.ServeHTTP(w, r)
 }
 
 type HTTPServer struct {
 	http.Server
+	headers map[string]string
 }
 
 func (h *HTTPServer) ListenAndServe() error {
 	logging.Info(
-		"Starting server on \033[%dm%s\033[0m at \033[%dm%s\033[0m",
-		logging.CYAN,
-		h.Server.Addr,
-		logging.CYAN,
-		*path,
+		"Starting server on %s at %s",
+		colorize(logging.CYAN, h.Server.Addr),
+		colorize(logging.CYAN, *path),
 	)
 
+	for k, v := range h.headers {
+		logging.Info("Adding Header: %s:%s",
+			colorize(logging.CYAN, k),
+			colorize(logging.CYAN, v),
+		)
+	}
+
 	return h.Server.ListenAndServe()
+}
+
+func colorize(color logging.Color, s string) string {
+	return fmt.Sprintf("\033[%dm%s\033[0m", color, s)
 }
